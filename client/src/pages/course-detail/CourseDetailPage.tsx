4d48934772e0da3f337b249aa526c3c15c39a1d5
손종민 <kain9101@naver.com>
2024-03-18 12:25:09 +0900

    Feat: MapTest 테스트 컴포넌트 생성

--- client/src/pages/course-detail/CourseDetailPage.tsx
+++ client/src/pages/course-detail/CourseDetailPage.tsx
@@ -1,7 +1,27 @@
+import { useNavigate, useParams } from "react-router";
+import { useState } from "react";
+import UseCurrentLocation from "./map-test/UseCurrentLocation";
+import MapTest from "./map-test/MapTest";
+
 function CourseDetailPage() {
+    const [lat, setLat] = useState(35.309216);
+    const [lon, setLon] = useState(129.0633216);
+    const { courseId } = useParams();
+    const navigate = useNavigate();
+    console.log(lat);
+    console.log(lon);
+    // 빈 배열을 전달하여 컴포넌트가 처음 렌더링될 때 한 번만 실행됩니다.
+
     return (
         <>
             <p>코스 조회 페이지</p>
+            <div>
+                <UseCurrentLocation setLat={setLat} setLon={setLon} />
+                <MapTest lat={lat} lon={lon} />
+            </div>
+            <button onClick={() => navigate(`/course/${courseId}/drive`)}>
+                주행하기
+            </button>
         </>
     );
 }
