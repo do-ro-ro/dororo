85e5274e5c00b04f5e565b1ca2af76860f5c0568
진성민 <sengmin14@naver.com>
2024-03-18 15:10:47 +0900

    Fix: index title 변경

--- client/src/pages/course-detail/CourseDetailPage.tsx
+++ client/src/pages/course-detail/CourseDetailPage.tsx
@@ -4,13 +4,18 @@ import UseCurrentLocation from "./map-test/UseCurrentLocation";
 import MapTest from "./map-test/MapTest";
 
 function CourseDetailPage() {
-    const [lat, setLat] = useState(35.309216);
-    const [lon, setLon] = useState(129.0633216);
+    const [lat, setLat] = useState<number>(35.309216);
+    const [lon, setLon] = useState<number>(129.0633216);
     const { courseId } = useParams();
     const navigate = useNavigate();
     console.log(lat);
     console.log(lon);
     // 빈 배열을 전달하여 컴포넌트가 처음 렌더링될 때 한 번만 실행됩니다.
+
+    // courseId가 null인 경우를 처리
+    if (!courseId) {
+        return <p>코스 ID를 찾을 수 없습니다.</p>;
+    }
 
     return (
         <>
