0ea8fd52df3aa037c5b0b20ab97f4ba93009efff
moonwlgh <moonwlgh@gmail.com>
2024-03-18 15:31:35 +0900

    Feat: 네이버 이미지 추가 및 lading 페이지에 등록

--- client/src/pages/landing-page/LoginButton.tsx
+++ client/src/pages/landing-page/LoginButton.tsx
@@ -1,5 +1,11 @@
+import naverLogin from "../../assets/naverLogin.png";
+
 const LoginButton = () => {
-    return <></>;
+    return (
+        <div>
+            <img src={naverLogin} alt="로고" className="p-20 pt-10" />
+        </div>
+    );
 };
 
 export default LoginButton;
