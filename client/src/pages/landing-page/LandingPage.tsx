af293b2548bcf429eb6ce1f365c194f5e9a235fd
moonwlgh <moonwlgh@gmail.com>
2024-03-18 15:18:32 +0900

    Feat: 부제 적용

--- client/src/pages/landing-page/LandingPage.tsx
+++ client/src/pages/landing-page/LandingPage.tsx
@@ -4,8 +4,10 @@ import logoWhite from "../../assets/logo_white.png";
 function LandingPage() {
     return (
         <div className="bg-dororoBlue w-full h-screen flex flex-col justify-center items-center">
-            <img src={logoWhite} alt="로고" className="p-10" />
-            <p></p>
+            <img src={logoWhite} alt="로고" className="p-10 pb-3" />
+            <p className="text-white text-base font-black">
+                빠르게 찾아보는 나만의 운전연습 코스
+            </p>
             <LoginButton></LoginButton>
         </div>
     );
