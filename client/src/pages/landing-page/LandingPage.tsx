3165e5d685b3c20a4fd9360069c5a70743286f65
moonwlgh <moonwlgh@gmail.com>
2024-03-18 14:32:58 +0900

    Fix: index.css 수정 및 dororoBlue 색상 설정

--- client/src/pages/landing-page/LandingPage.tsx
+++ client/src/pages/landing-page/LandingPage.tsx
@@ -2,10 +2,11 @@ import LoginButton from "./LoginButton";
 
 function LandingPage() {
     return (
-        <>
+        // Tailwind CSS 클래스를 사용하여 최상위 div에 배경색, 너비, 최소 높이 설정
+        <div className="bg-dororoBlue w-full h-screen flex flex-col justify-center items-center">
             <p>랜딩 페이지</p>
             <LoginButton></LoginButton>
-        </>
+        </div>
     );
 }
 
