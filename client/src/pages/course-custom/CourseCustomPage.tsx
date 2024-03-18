d32db37198c50d4c6d064ae6256597bf90170f85
손종민 <kain9101@naver.com>
2024-03-18 16:22:23 +0900

    Chore: mui/icons-material 설치

--- client/src/pages/course-custom/CourseCustomPage.tsx
+++ client/src/pages/course-custom/CourseCustomPage.tsx
@@ -1,7 +1,14 @@
+import { Box } from "@mui/material";
+import Drawer from "./Drawer";
+
 function CourseCustomPage() {
+    const dummyMap = { map_name: "테스트코스" };
     return (
         <>
-            <p>코스 커스텀 페이지</p>
+            <div>{dummyMap.map_name} 수정하기</div>
+            <div>지도 들어갈 영역</div>
+            <Drawer />
+            <Box>푸터바</Box>
         </>
     );
 }
