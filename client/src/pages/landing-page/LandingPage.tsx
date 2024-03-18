a32025a0fed97a27396aeaa9c22b8c86cde156e9
moonwlgh <moonwlgh@gmail.com>
2024-03-18 14:06:23 +0900

    Init : assets 폴더 생성 및 로고이미지 추가

--- client/src/pages/landing-page/LandingPage.tsx
+++ client/src/pages/landing-page/LandingPage.tsx
@@ -1,7 +1,10 @@
+import LoginButton from "./LoginButton";
+
 function LandingPage() {
     return (
         <>
             <p>랜딩 페이지</p>
+            <LoginButton></LoginButton>
         </>
     );
 }
