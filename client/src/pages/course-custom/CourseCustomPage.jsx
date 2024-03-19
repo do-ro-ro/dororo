319fc9834c22651f7602c2f678ac37a2623fdf99
jongmin Son <kain9101@naver.com>
2024-03-19 22:51:32 +0900

    Feat: CourseCustomPage.jsx

--- client/src/pages/course-custom/CourseCustomPage.jsx
+++ client/src/pages/course-custom/CourseCustomPage.jsx
@@ -1,5 +1,29 @@
+import { Box } from "@mui/material";
+import Drawer from "./Drawer";
+import Map from "./Map";
+import { useState } from "react";
+import FooterBar from "./FooterBar";
+
 function CourseCustomPage() {
-    return <></>;
+    const dummyMap = { map_name: "테스트코스" };
+    const [lat] = useState(35.309216);
+    const [lon] = useState(129.033216);
+    return (
+        <>
+            <Box
+                bgcolor={"white"}
+                border={"grey"}
+                boxShadow="2px"
+                textAlign={"center"}
+                py="2vh"
+            >
+                {dummyMap.map_name} 수정하기
+            </Box>
+            <Map lat={lat} lon={lon} />
+            <Drawer />
+            {/* <FooterBar /> */}
+        </>
+    );
 }
 
 export default CourseCustomPage;
