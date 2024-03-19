91c48a9394f46e318af2e5fb15b2c227c9854321
문지호 <moonwlgh@gmail.com>
2024-03-19 23:39:31 +0900

    Feat: 현재위치버튼 구현 및 컴포넌트화

--- client/src/pages/recommend-course/RecommendCoursePage.jsx
+++ client/src/pages/recommend-course/RecommendCoursePage.jsx
@@ -1,5 +1,7 @@
 import Map from "../../components/map/Map";
+import CurrentLocation from "../../components/map/CurrentLocation";
 import { useState } from "react";
+
 function RecommendedCoursePage() {
     const [lat, setLat] = useState(37.5652045);
     const [lon, setLon] = useState(126.98702028);
@@ -7,6 +9,11 @@ function RecommendedCoursePage() {
     return (
         <div>
             <Map lat={lat} lon={lon}></Map>
+            <CurrentLocation
+                setLat={setLat}
+                setLon={setLon}
+                setTimestamp={setTimestamp}
+            ></CurrentLocation>
         </div>
     );
 }
