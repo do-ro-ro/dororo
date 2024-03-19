20bd1856110220136cbd9481eec896d68da6ac12
문지호 <moonwlgh@gmail.com>
2024-03-19 23:29:17 +0900

    Feat: 맵 구현 및 컴포넌트 화

--- client/src/pages/recommend-course/RecommendCoursePage.jsx
+++ client/src/pages/recommend-course/RecommendCoursePage.jsx
@@ -1,5 +1,14 @@
+import Map from "../../components/map/Map";
+import { useState } from "react";
 function RecommendedCoursePage() {
-    return <></>;
+    const [lat, setLat] = useState(37.5652045);
+    const [lon, setLon] = useState(126.98702028);
+    const [timestamp, setTimestamp] = useState();
+    return (
+        <div>
+            <Map lat={lat} lon={lon}></Map>
+        </div>
+    );
 }
 
 export default RecommendedCoursePage;
