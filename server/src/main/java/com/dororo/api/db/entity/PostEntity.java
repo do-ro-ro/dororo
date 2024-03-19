bcc5afc88777f7606bdbe68865e6b907df35113e
kimmin <kimmin5714@gmail.com>
2024-03-19 12:05:21 +0900

    Fix: Entity 외래키 설정

--- server/src/main/java/com/dororo/api/db/entity/PostEntity.java
+++ server/src/main/java/com/dororo/api/db/entity/PostEntity.java
@@ -2,11 +2,18 @@ package com.dororo.api.db.entity;
 
 
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -24,11 +31,18 @@ public class PostEntity {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer postId;
 
-	private Integer mapId;
+	@OneToOne(targetEntity = MapEntity.class)
+	@JoinColumn(name = "mapId")
+	private MapEntity mapId;
 	private Integer userId;
 	private String postTitle;
 	private String postContent;
 	private Timestamp createdAt;
 	private Timestamp updatedAt;
 	private Integer scrapCount;
+
+	@ElementCollection
+	@CollectionTable(name = "reviews", joinColumns = @JoinColumn(name = "post_id"))
+	@Column(name="reviewRef")
+	private List<String> reviewRefs = new ArrayList<>();
 }
