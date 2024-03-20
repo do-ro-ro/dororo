501c21715bee0b00206e995476c4213b2aba96d6
jongmin Son <kain9101@naver.com>
2024-03-20 23:39:58 +0900

    Refactor: BasicNavbar

---
+++ client/src/components/navbar/BasicNavbar.jsx
@@ -0,0 +1,40 @@
+import { AccountBox, AddRoad, Forum } from "@mui/icons-material";
+import {
+    BottomNavigation,
+    BottomNavigationAction,
+    Box,
+    Paper,
+} from "@mui/material";
+import { useState } from "react";
+
+function BasicNavbar() {
+    const [value, setValue] = useState(1);
+    return (
+        <>
+            <Paper
+                sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
+                elevation={3}
+            >
+                <BottomNavigation
+                    showLabels
+                    value={value}
+                    onChange={(event, newValue) => {
+                        setValue(newValue);
+                    }}
+                >
+                    <BottomNavigationAction label="커뮤니티" icon={<Forum />} />
+                    <BottomNavigationAction
+                        label="코스추천"
+                        icon={<AddRoad />}
+                    />
+                    <BottomNavigationAction
+                        label="마이페이지"
+                        icon={<AccountBox />}
+                    />
+                </BottomNavigation>
+            </Paper>
+        </>
+    );
+}
+
+export default BasicNavbar;
--- client/src/pages/community-list/CommunityListPage.jsx
+++ client/src/pages/community-list/CommunityListPage.jsx
@@ -1,42 +1,11 @@
-import { AccountBox, AddRoad, Forum } from "@mui/icons-material";
-import {
-    BottomNavigation,
-    BottomNavigationAction,
-    Box,
-    Paper,
-} from "@mui/material";
-import { useState } from "react";
+import { Box } from "@mui/material";
+import BasicNavbar from "../../components/navbar/BasicNavbar";
 
 function CommunityListPage() {
-    const [value, setValue] = useState(1);
     return (
         <>
             <Box>
-                <Paper
-                    sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
-                    elevation={3}
-                >
-                    <BottomNavigation
-                        showLabels
-                        value={value}
-                        onChange={(event, newValue) => {
-                            setValue(newValue);
-                        }}
-                    >
-                        <BottomNavigationAction
-                            label="커뮤니티"
-                            icon={<Forum />}
-                        />
-                        <BottomNavigationAction
-                            label="코스추천"
-                            icon={<AddRoad />}
-                        />
-                        <BottomNavigationAction
-                            label="마이페이지"
-                            icon={<AccountBox />}
-                        />
-                    </BottomNavigation>
-                </Paper>
+                <BasicNavbar />
             </Box>
         </>
     );
