f4004e7bf55a9cc851fd5cacb1770c5eaa123f2c
jongmin Son <kain9101@naver.com>
2024-03-20 23:51:50 +0900

    Feat: ColorTheme

---
+++ client/src/components/theme/Palette.jsx
@@ -0,0 +1,11 @@
+import { createTheme } from "@mui/material";
+
+const colorTheme = createTheme({
+    palette: {
+        primary: {
+            main: "#FF5733",
+        },
+    },
+});
+
+export default colorTheme;
