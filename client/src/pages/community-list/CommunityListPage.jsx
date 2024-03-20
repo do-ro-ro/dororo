3f5dc43c634cac5e2ae36ac885a7e9668f729197
jongmin Son <kain9101@naver.com>
2024-03-20 23:37:47 +0900

    Feat: Navbar in CommunityListPage

--- client/src/pages/community-list/CommunityListPage.jsx
+++ client/src/pages/community-list/CommunityListPage.jsx
@@ -1,5 +1,45 @@
+import { AccountBox, AddRoad, Forum } from "@mui/icons-material";
+import {
+    BottomNavigation,
+    BottomNavigationAction,
+    Box,
+    Paper,
+} from "@mui/material";
+import { useState } from "react";
+
 function CommunityListPage() {
-    return <></>;
+    const [value, setValue] = useState(1);
+    return (
+        <>
+            <Box>
+                <Paper
+                    sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
+                    elevation={3}
+                >
+                    <BottomNavigation
+                        showLabels
+                        value={value}
+                        onChange={(event, newValue) => {
+                            setValue(newValue);
+                        }}
+                    >
+                        <BottomNavigationAction
+                            label="커뮤니티"
+                            icon={<Forum />}
+                        />
+                        <BottomNavigationAction
+                            label="코스추천"
+                            icon={<AddRoad />}
+                        />
+                        <BottomNavigationAction
+                            label="마이페이지"
+                            icon={<AccountBox />}
+                        />
+                    </BottomNavigation>
+                </Paper>
+            </Box>
+        </>
+    );
 }
 
 export default CommunityListPage;
