9131147d7aed0acc0d11a02eacf2875805426f5d
김영후 <eagle7449@naver.com>
2024-03-22 00:44:27 +0900

    Feat: PostDto, GetPostDetailsDto 필드 선언 및 명세 작성

--- server/src/main/java/com/dororo/api/commnunity/dto/PostDto.java
+++ server/src/main/java/com/dororo/api/commnunity/dto/PostDto.java
@@ -1,21 +1,19 @@
 package com.dororo.api.commnunity.dto;
 
-import com.dororo.api.db.entity.MapEntity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-
 import java.sql.Timestamp;
 import java.util.List;
 
 @AllArgsConstructor
 @Getter
 @Setter
-public class PostDto {
+public class PostDto {  // Post Entity에 대응하는 필드를 모두 가지고 있는 dto
 
     private Integer postId;
 
-    private MapEntity mapId;
+    private Integer mapId;  // dto에서는 Integer로 맵ID를 나름
     private String postTitle;
     private String postContent;
     private Timestamp createdAt;
--- server/src/main/java/com/dororo/api/commnunity/dto/response/GetPostDetailsDto.java
+++ server/src/main/java/com/dororo/api/commnunity/dto/response/GetPostDetailsDto.java
@@ -1,4 +1,25 @@
 package com.dororo.api.commnunity.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor  // 틀 작성하는 시점에서는 기본 생성자를 두지만, 로직 작성 완료 후 필요 없으면 지울 것
+@AllArgsConstructor
+@Getter
+@Setter
 public class GetPostDetailsDto {
+
+    private Integer postId;
+
+    @Schema(description = "Post의 원본 맵 ID")
+    private Integer mapId;
+    @Schema(description = "S3 버킷에 저장된 맵 이미지의 주소")
+    private String mapImage;
+    private String postTitle;
+    private String postContent;
+    private int scrapCount;
+
 }
