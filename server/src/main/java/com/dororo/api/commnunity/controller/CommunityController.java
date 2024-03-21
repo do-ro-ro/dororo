f0daf66ac495a99b7b6328049530c9b0ef6a1f73
김영후 <eagle7449@naver.com>
2024-03-21 17:50:10 +0900

    Feat: 커뮤니티 게시글 작성 API 명세 틀 작성

--- server/src/main/java/com/dororo/api/commnunity/controller/CommunityController.java
+++ server/src/main/java/com/dororo/api/commnunity/controller/CommunityController.java
@@ -1,6 +1,9 @@
 package com.dororo.api.commnunity.controller;
 
 import com.dororo.api.commnunity.service.CommunityService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,6 +20,11 @@ public class CommunityController {
     
     private CommunityService communityService;
 
+    // <-------------------- POST part -------------------->
+    @Operation(summary = "커뮤니티 게시글 생성 요청", description = "코스 공유를 했을 때 동작을 수행하는 API입니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "코스 공유 성공")
+    })
     @PostMapping("")
     public ResponseEntity addPosts() {
 
