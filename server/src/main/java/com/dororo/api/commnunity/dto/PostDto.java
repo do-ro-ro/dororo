d6c790da20bb1f14d5b19caae7bad5bdd23f27b6
김영후 <eagle7449@naver.com>
2024-03-22 00:36:14 +0900

    Feat: PostDto에 필드들 추가

--- server/src/main/java/com/dororo/api/commnunity/dto/PostDto.java
+++ server/src/main/java/com/dororo/api/commnunity/dto/PostDto.java
@@ -1,4 +1,27 @@
 package com.dororo.api.commnunity.dto;
 
+import com.dororo.api.db.entity.MapEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+@AllArgsConstructor
+@Getter
+@Setter
 public class PostDto {
+
+    private Integer postId;
+
+    private MapEntity mapId;
+    private String postTitle;
+    private String postContent;
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
+    private int scrapCount;
+
+    private List<String> reviewRefs;
+
 }
