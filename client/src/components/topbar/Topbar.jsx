c45b9be57a6670d06962e507a8ba87d54ce1edda
손종민 <kain9101@naver.com>
2024-03-21 09:59:43 +0900

    Feat: Topbar

--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -1,6 +1,27 @@
+import {
+    AppBar,
+    Box,
+    Container,
+    IconButton,
+    Toolbar,
+    Typography,
+} from "@mui/material";
+
 function Topbar(props) {
     const { children, pageInfo, isBackButton, ...rest } = props;
-    return <></>;
+    return (
+        <>
+            <Box sx={{ flexGrow: 1 }}>
+                <AppBar position="static">
+                    <Container maxWidth="xl">
+                        <Toolbar>
+                            <Typography {...rest}>{children}</Typography>
+                        </Toolbar>
+                    </Container>
+                </AppBar>
+            </Box>
+        </>
+    );
 }
 
 export default Topbar;
