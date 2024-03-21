21292660bca8d71045950dd1fcce0b2cba9f5425
손종민 <kain9101@naver.com>
2024-03-21 17:42:23 +0900

    Fix: Snackbar 컴포넌트 비우기

--- client/src/components/community-detail/Snackbar.jsx
+++ client/src/components/community-detail/Snackbar.jsx
@@ -3,13 +3,7 @@ import { Snackbar, Alert } from "@mui/material";
 function DororoSnackbar(props) {
     const { open, onClose, ...rest } = props;
 
-    return (
-        <>
-            <Snackbar open={open}>
-                <Alert onClose={onClose}>마이페이지로 스크랩되었습니다</Alert>
-            </Snackbar>
-        </>
-    );
+    return <></>;
 }
 
 export default DororoSnackbar;
