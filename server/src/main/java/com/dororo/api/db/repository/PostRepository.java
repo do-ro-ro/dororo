a65a12578eae5d1bf4670ae4bfb8346be1b9a534
김영후 <eagle7449@naver.com>
2024-03-22 10:41:26 +0900

    Feat: Post repository postId를 통한 조회 메서드 선언

--- server/src/main/java/com/dororo/api/db/repository/PostRepository.java
+++ server/src/main/java/com/dororo/api/db/repository/PostRepository.java
@@ -4,6 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.dororo.api.db.entity.PostEntity;
 
+import java.util.Optional;
+
 @Repository
 public interface PostRepository extends JpaRepository<PostEntity, Integer> {
+
+    Optional<PostEntity> findByPostId(Integer postId);  // isPresent() 메서드 사용을 통한 예외 처리를 위해 Optional type으로 선언
+
 }
