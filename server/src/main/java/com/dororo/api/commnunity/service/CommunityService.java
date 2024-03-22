7fd11fee252749caff560eb1bbecb1d6ded36c6a
김영후 <eagle7449@naver.com>
2024-03-22 10:25:45 +0900

    Feat: ModelMapper 의존성 추가

--- server/src/main/java/com/dororo/api/commnunity/service/CommunityService.java
+++ server/src/main/java/com/dororo/api/commnunity/service/CommunityService.java
@@ -2,11 +2,16 @@ package com.dororo.api.commnunity.service;
 
 import com.dororo.api.commnunity.dto.response.GetPostDetailsDto;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class CommunityService {
+
+    @Autowired
+    ModelMapper modelMapper;    // Entity -> Dto 간 변환에 사용
 
     public void addPost() {
 
