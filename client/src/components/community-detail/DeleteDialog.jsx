10d359aeecd5bf65ff897c01a20bedf4dea79386
jongmin Son <kain9101@naver.com>
2024-03-24 23:44:27 +0900

    Feat: Delete Dialog

--- client/src/components/community-detail/DeleteDialog.jsx
+++ client/src/components/community-detail/DeleteDialog.jsx
@@ -1,5 +1,54 @@
-function DeleteDialog() {
-    return <></>;
+import {
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+} from "@mui/material";
+import { useState } from "react";
+
+function DeleteDialog(props) {
+    const [open, setOpen] = useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <>
+            <Button variant="contained" color="error" sx={{ height: "3vh" }}>
+                삭제
+            </Button>
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">
+                    {"Use Google's location service?"}
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        Let Google help apps determine location. This means
+                        sending anonymous location data to Google, even when no
+                        apps are running.
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>Disagree</Button>
+                    <Button onClick={handleClose} autoFocus>
+                        Agree
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </>
+    );
 }
 
 export default DeleteDialog;
