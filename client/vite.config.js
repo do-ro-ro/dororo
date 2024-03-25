1c96fc380d7913987542bdfe6b65c83f9da164e9
손종민 <kain9101@naver.com>
2024-03-25 15:54:13 +0900

    Fix: vite.config.js 설정 수정, 빌드 확인

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,27 +7,33 @@ export default defineConfig({
     plugins: [
         react(),
         VitePWA({
+            includeAssets: [
+                "favicon.ico",
+                "maskable_icon_x192.png",
+                "maskable_icon_x512.png",
+            ],
             registerType: "autoUpdate",
             devOptions: { enabled: true },
             manifest: {
                 name: "도로로",
                 short_name: "도로로",
+                description: "지금 바로 도로로!",
                 theme_color: "#6386BE",
                 background_color: "#6386BE",
                 icons: [
                     {
-                        src: "./public/maskable_icon_x192.png",
+                        src: "maskable_icon_x192.png",
                         type: "image/png",
                         sizes: "192x192",
                     },
                     {
-                        src: "./public/maskable_icon_x512.png",
+                        src: "maskable_icon_x512.png",
                         type: "image/png",
                         sizes: "512x512",
                         purpose: "maskable",
                     },
                     {
-                        src: "./public/maskable_icon_x512.png",
+                        src: "maskable_icon_x512.png",
                         type: "image/png",
                         sizes: "512x512",
                         purpose: "any",
