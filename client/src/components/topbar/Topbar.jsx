306ab8c763fac81d887184de13aeb199d57c1c69
jongmin Son <kain9101@naver.com>
2024-03-25 23:35:46 +0900

    Fix: Topbar 색상 수정, 뒤로 버튼 navigate 적용

--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -7,17 +7,38 @@ import {
     Toolbar,
     Typography,
 } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 function Topbar(props) {
     const { children, pageInfo, isBackButton, ...rest } = props;
+
+    const navigate = useNavigate();
+
     return (
         <>
             <Box sx={{ flexGrow: 1 }}>
-                <AppBar position="static">
+                <AppBar position="static" color="inherit">
                     <Container maxWidth="xl">
                         <Toolbar>
-                            <ArrowBack />
-                            <Typography {...rest}>{children}</Typography>
+                            {isBackButton ? (
+                                <IconButton
+                                    edge="start"
+                                    color="inherit"
+                                    onClick={() => navigate(-1)}
+                                >
+                                    <ArrowBack />
+                                </IconButton>
+                            ) : null}
+                            <Typography
+                                {...rest}
+                                variant="h6"
+                                sx={{ flexGrow: 1, textAlign: "center" }}
+                            >
+                                {children}
+                            </Typography>
+                            {isBackButton ? (
+                                <div style={{ width: 24 }} />
+                            ) : null}
                         </Toolbar>
                     </Container>
                 </AppBar>
