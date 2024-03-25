ac26180215045c561843a2120a75fbe602b5b062
손종민 <kain9101@naver.com>
2024-03-25 11:46:18 +0900

    Feat: PWA 적용

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,30 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: "autoUpdate",
+            devOptions: { enabled: true },
+            manifest: {
+                icons: [
+                    {
+                        src: "./public/favicon-196x196.png",
+                        type: "image/png",
+                        sizes: "196x196",
+                    },
+                    {
+                        src: "./public/mstile-310x310.png",
+                        type: "image/png",
+                        sizes: "310x310",
+                    },
+                ],
+            },
+        }),
+    ],
     // resolve: {
     //     alias: { "@": Path2D.resolve(__dirname, "./src") },
     // },
