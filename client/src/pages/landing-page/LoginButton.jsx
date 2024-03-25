e3e3b88a77c3964338bd6a6b22bca778d033615d
손종민 <kain9101@naver.com>
2024-03-25 16:12:36 +0900

    Fix: LoginButton 테스트 위한 navigate 추가

--- client/src/pages/landing-page/LoginButton.jsx
+++ client/src/pages/landing-page/LoginButton.jsx
@@ -1,9 +1,14 @@
+import { useNavigate } from "react-router-dom";
 import naverLogin from "../../assets/naverLogin.png";
 
 const LoginButton = () => {
+    const navigate = useNavigate();
     return (
         <div>
-            <button>
+            <button
+                // 임시 onClick - 메인화면으로 navigate
+                onClick={() => navigate("/main")}
+            >
                 <img src={naverLogin} alt="로고" className="p-20 pt-10" />
             </button>
         </div>
