7a43e8101364be4fdfeb9ecd7b1bd7b147adaa11
moonwlgh <moonwlgh@gmail.com>
2024-03-27 15:11:35 +0900

    Feat: courseInfo UI/UX 적용

--- client/src/pages/recommend-course/CourseInfo.jsx
+++ client/src/pages/recommend-course/CourseInfo.jsx
@@ -1,12 +1,75 @@
+import { Box, Typography, Button } from "@mui/material";
+
 const CourseInfo = ({ courseInfo, currentIndex }) => {
     return (
-        <div>
-            <div>
-                <p>코스 {currentIndex + 1}</p>
-                <p>주행시간{courseInfo.time}</p>
-                <p>주행 거리 {courseInfo.distance}</p>
-            </div>
-        </div>
+        <>
+            <Box
+                sx={{
+                    width: "100%",
+                    height: "16vh",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                }}
+            >
+                <Box
+                    sx={{
+                        height: "8vh",
+                        display: "flex",
+                        alignItems: "center",
+                    }}
+                >
+                    <Typography
+                        variant="h4"
+                        component="h4"
+                        sx={{ fontWeight: 700, ml: 2 }}
+                    >
+                        코스 {currentIndex + 1}
+                    </Typography>
+                </Box>
+
+                <Box
+                    sx={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                        height: "8vh",
+                        marginX: "2vh",
+                        textAlign: "center",
+                    }}
+                >
+                    <Box>
+                        <Box>
+                            <Typography>주행 시간</Typography>
+                            <Typography
+                                variant="h5"
+                                component="h5"
+                                sx={{ fontWeight: 600 }}
+                            >
+                                {courseInfo.time}
+                            </Typography>
+                        </Box>
+                    </Box>
+                    <Box>
+                        <Box>
+                            <Typography>주행 거리</Typography>
+                            <Typography
+                                variant="h5"
+                                component="h5"
+                                sx={{ fontWeight: 600 }}
+                            >
+                                {courseInfo.distance}
+                            </Typography>
+                        </Box>
+                    </Box>
+                    <Box>
+                        <Button variant="contained" sx={{ fontWeight: "bold" }}>
+                            경유지 상세
+                        </Button>
+                    </Box>
+                </Box>
+            </Box>
+        </>
     );
 };
 
