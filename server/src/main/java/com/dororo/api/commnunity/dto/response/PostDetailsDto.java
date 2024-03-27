ba1ba4a2c8b2055d238f706864a8df76585e3596
김영후 <eagle7449@naver.com>
2024-03-27 23:06:39 +0900

    Fix: PostResponseDto의 mapId 타입 MapEntity로 변경, response dto에 modelMapper를 위한 Setter 설정

--- server/src/main/java/com/dororo/api/commnunity/dto/response/PostDetailsDto.java
+++ server/src/main/java/com/dororo/api/commnunity/dto/response/PostDetailsDto.java
@@ -2,11 +2,14 @@ package com.dororo.api.commnunity.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.locationtech.jts.geom.LineString;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
+@Setter
 public class PostDetailsDto extends PostResponseDto {
 
     private String postTitle;
--- server/src/main/java/com/dororo/api/commnunity/dto/response/PostResponseDto.java
+++ server/src/main/java/com/dororo/api/commnunity/dto/response/PostResponseDto.java
@@ -1,17 +1,20 @@
 package com.dororo.api.commnunity.dto.response;
 
+import com.dororo.api.db.entity.MapEntity;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.sql.Timestamp;
 
 @Getter
+@Setter
 public abstract class PostResponseDto { // post의 response에서 공통으로 반환하는 값들을 선언한 추상 클래스
 
     protected Integer postId;
     
     @Schema(description = "Post의 원본 맵 ID, 스크랩 기능에 필요함")
-    protected Integer mapId;
+    protected MapEntity mapId;
     @Schema(description = "S3 버킷에 저장된 맵 이미지의 주소")
     protected String mapImage;
     protected String userName;    // 게시글 작성자 명
