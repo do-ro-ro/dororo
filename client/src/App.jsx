f9a093e30f105a3276e669363c2160fbb9a83845
moonwlgh <moonwlgh@gmail.com>
2024-03-27 11:31:14 +0900

    Fix: 라우팅 경로 수정

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,6 +12,7 @@ import CommunityDetailPage from "./pages/community-detail/CommunityDetailPage";
 import MyPage from "./pages/my-page/MyPage";
 import { ThemeProvider, createTheme } from "@mui/material";
 import MainPage from "./components/navbar/MainPage";
+import Main from "./pages/main-page/Main";
 
 import DriveTest from "./components/course-drive/DriveTest/DriveTest";
 
@@ -33,10 +34,7 @@ function App() {
                         <Route path="/" element={<LandingPage />} />
                         <Route path={"/main/*"} element={<MainPage />}>
                             {/* /main으로 접근했을 때 자동으로 /recommend로 리다이렉트 */}
-                            <Route
-                                path=""
-                                element={<Navigate to="recommend" />}
-                            />
+                            <Route path="" element={<Main />} />
                             <Route
                                 path="recommend"
                                 element={<RecommendedCoursePage />}
--- client/src/pages/recommend-course/RecommendCoursePage.jsx
+++ client/src/pages/recommend-course/RecommendCoursePage.jsx
@@ -1,67 +1,67 @@
-import { useState } from "react";
+// import { useState } from "react";
 
-import Map from "./Map";
-import CurrentLocation from "./CurrentLocation";
-import Search from "../main-page/Search";
-import RecommendButton from "../main-page/RecommendButton";
-import OptionModal from "./OptionModal";
+// import Map from "./Map";
+// import CurrentLocation from "./CurrentLocation";
+// import Search from "../main-page/Search";
+// import RecommendButton from "../main-page/RecommendButton";
+// import OptionModal from "./OptionModal";
 
 function RecommendedCoursePage() {
-    const [lat, setLat] = useState(37.5652045);
-    const [lon, setLon] = useState(126.98702028);
-    const [timestamp, setTimestamp] = useState();
+    // const [lat, setLat] = useState(37.5652045);
+    // const [lon, setLon] = useState(126.98702028);
+    // const [timestamp, setTimestamp] = useState();
 
-    const [option, setOption] = useState({
-        method: "",
-        length: 5,
-        left: 1,
-        right: 1,
-        uturn: 1,
-    });
+    // const [option, setOption] = useState({
+    //     method: "",
+    //     length: 5,
+    //     left: 1,
+    //     right: 1,
+    //     uturn: 1,
+    // });
 
-    const [showModal, setShowModal] = useState(false);
+    // const [showModal, setShowModal] = useState(false);
 
-    const openModal = () => {
-        setShowModal(true);
-    };
+    // const openModal = () => {
+    //     setShowModal(true);
+    // };
 
-    const closeModal = () => {
-        setShowModal(false);
-        console.log(option);
-    };
+    // const closeModal = () => {
+    //     setShowModal(false);
+    //     console.log(option);
+    // };
 
     return (
-        <>
+        <div>
             {/* <div className="relative "> */}
-            <div>
-                <Search setLat={setLat} setLon={setLon}></Search>
-            </div>
+            <div>{/* <Search setLat={setLat} setLon={setLon}></Search> */}</div>
             <div className="relative ">
                 {/* 지도에 대한 컨테이너 */}
-                <Map lat={lat} lon={lon}></Map>
+                {/* <Map lat={lat} lon={lon}></Map> */}
                 <div className="absolute bottom-40 left-1/2 transform -translate-x-1/2 mb-2">
-                    <RecommendButton openModal={openModal}></RecommendButton>
+                    {/* <RecommendButton openModal={openModal}></RecommendButton> */}
                 </div>
             </div>
             <div className="absolute top-24 right-0  m-4">
                 {/* CurrentLocation의 위치를 지정 */}
-                <CurrentLocation
+                {/* <CurrentLocation
                     setLat={setLat}
                     setLon={setLon}
                     setTimestamp={setTimestamp}
-                ></CurrentLocation>
+                ></CurrentLocation> */}
             </div>
             <div>
-                {showModal && (
+                {/* {showModal && (
                     <OptionModal
                         open={showModal}
                         closeModal={closeModal}
                         setOption={setOption}
                         option={option}
                     />
-                )}
+                )} */}
             </div>
-        </>
+
+            <div>dd</div>
+        </div>
     );
 }
 
