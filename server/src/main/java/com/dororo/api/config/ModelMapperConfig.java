df93c0913187e9f0ce1a6899e39f7286a017081c
김영후 <eagle7449@naver.com>
2024-03-28 10:33:25 +0900

    Fix: 상세 조회 dto 유저 이름 -> 유저 닉네임 반환으로 수정

--- server/src/main/java/com/dororo/api/config/ModelMapperConfig.java
+++ server/src/main/java/com/dororo/api/config/ModelMapperConfig.java
@@ -21,7 +21,7 @@ public class ModelMapperConfig {
             @Override
             protected void configure() {
                 map().setMapId(source.getMapId().getMapId()); // MapEntity의 mapId를 추출하여 설정
-                map().setUserName(source.getMapId().getUserId().getName());
+                map().setUserNickName(source.getMapId().getUserId().getNickname());
             }
         });
 
