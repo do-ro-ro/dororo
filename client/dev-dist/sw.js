9812c7eb662bbff850d82a9af42685588ac55d88
문지호 <moonwlgh@gmail.com>
2024-03-29 00:14:11 +0900

    Feat: sns 로그인 구현

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.0p64g0e225"
+    "revision": "0.0op4po4m668"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- client/src/pages/oauth-page/OauthPage.jsx
+++ client/src/pages/oauth-page/OauthPage.jsx
@@ -1,5 +1,42 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { jwtDecode } from "jwt-decode";
+
 const OauthPage = () => {
-    return <></>;
+    const [user, setUser] = useState({});
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const queryString = window.location.search;
+        const urlParams = new URLSearchParams(queryString);
+        const accessToken = urlParams.get("access");
+        const refreshToken = urlParams.get("refresh");
+
+        if (accessToken && refreshToken) {
+            localStorage.setItem("accessToken", accessToken);
+            localStorage.setItem("refreshToken", refreshToken);
+        }
+
+        if (accessToken) {
+            try {
+                const decoded = jwtDecode(accessToken);
+                setUser(decoded); // 토큰에서 추출한 사용자 정보를 저장
+                console.log(decoded);
+                console.log(user);
+            } catch (error) {
+                console.error("Error decoding token:", error);
+            }
+        }
+    }, []);
+
+    useEffect(() => {
+        if (user) navigate("/main");
+        else navigate("/");
+    }, [user, navigate]);
+
+    return <div>Oauth Redirect Page</div>;
 };
 
 export default OauthPage;
