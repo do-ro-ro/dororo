b649b8ad1c150b16b5900f119b9e3c30a23d3d36
손종민 <kain9101@naver.com>
2024-03-28 10:43:24 +0900

    Fix: Navbar 상태 튀는 부분 수정

--- client/src/components/navbar/MainPage.jsx
+++ client/src/components/navbar/MainPage.jsx
@@ -1,4 +1,4 @@
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet, useLocation, useParams } from "react-router-dom";
 import BasicNavbar from "./BasicNavbar";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
@@ -7,6 +7,7 @@ import { setValue } from "../../features/basicNavbarSlice";
 function MainPage() {
     const dispatch = useDispatch();
     const location = useLocation();
+    const { postId } = useParams();
 
     useEffect(() => {
         // URL에 따라 네브바 값 설정
@@ -14,14 +15,14 @@ function MainPage() {
             case "/main/community":
                 dispatch(setValue(0));
                 break;
+            case `/main/community/${postId}`:
+                dispatch(setValue(0));
+                break;
             case "/main/recommend":
                 dispatch(setValue(1));
                 break;
             case `/main/myPage/123`: // 예시 userId
                 dispatch(setValue(2));
-                break;
-            default:
-                dispatch(setValue(1)); // 기본값 설정
                 break;
         }
     }, [location.pathname, dispatch]);
