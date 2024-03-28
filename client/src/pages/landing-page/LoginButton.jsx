4785665832e903db89ffe916948e83b8acb85e38
문지호 <moonwlgh@gmail.com>
2024-03-28 22:36:44 +0900

    Feat: handleNaverLogin 생성

--- client/src/pages/landing-page/LoginButton.jsx
+++ client/src/pages/landing-page/LoginButton.jsx
@@ -1,13 +1,15 @@
-import { useNavigate } from "react-router-dom";
 import naverLogin from "../../assets/naverLogin.png";
 
 const LoginButton = () => {
-    const navigate = useNavigate();
+    const handleNaverLogin = () => {
+        window.location.href =
+            "https://j10e202.p.ssafy.io/api/auth/oauth2/naver";
+    };
     return (
         <div>
             <button
                 // 임시 onClick - 메인화면으로 navigate
-                onClick={() => navigate("/main")}
+                onClick={handleNaverLogin}
             >
                 <img src={naverLogin} alt="로고" className="p-20 pt-10" />
             </button>
