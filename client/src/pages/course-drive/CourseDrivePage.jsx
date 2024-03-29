c17b1a346c764c8a0eed418c0d90b7822c9cdc2f
진성민 <sengmin14@naver.com>
2024-03-29 10:35:37 +0900

    feat : 드라비으 테스트용 폴더 제거

--- client/src/pages/course-drive/CourseDrivePage.jsx
+++ client/src/pages/course-drive/CourseDrivePage.jsx
@@ -34,42 +34,6 @@ function CourseDrivePage() {
     const [time, setTime] = useState(0);
     const [km, setKm] = useState(0);
 
-    const [lat1, setLat1] = useState(35.095737617642946);
-    const [lng1, setLng1] = useState(128.84941070168463);
-
-    const [lat2, setLat2] = useState(35.095737617642946);
-    const [lng2, setLng2] = useState(128.84941070168463);
-
-    const [lat3, setLat3] = useState(35.095737617642946);
-    const [lng3, setLng3] = useState(128.84941070168463);
-
-    const [lat4, setLat4] = useState(35.095737617642946);
-    const [lng4, setLng4] = useState(128.84941070168463);
-
-    const [lat5, setLat5] = useState(35.095737617642946);
-    const [lng5, setLng5] = useState(128.84941070168463);
-
-    const [lat6, setLat6] = useState(35.095737617642946);
-    const [lng6, setLng6] = useState(128.84941070168463);
-
-    const [lat7, setLat7] = useState(35.095737617642946);
-    const [lng7, setLng7] = useState(128.84941070168463);
-
-    const [lat8, setLat8] = useState(35.095737617642946);
-    const [lng8, setLng8] = useState(128.84941070168463);
-
-    const [lat9, setLat9] = useState(35.095737617642946);
-    const [lng9, setLng9] = useState(128.84941070168463);
-
-    const [lat10, setLat10] = useState(35.095737617642946);
-    const [lng10, setLng10] = useState(128.84941070168463);
-
-    const [watchLat1, setWatchLat1] = useState(35.095737617642946);
-    const [watchLng1, setWatchLng1] = useState(128.84941070168463);
-
-    const [googleLat, setGoogleLat] = useState(35.095737617642946);
-    const [googleLng, setGoogleLng] = useState(128.84941070168463);
-
     // 운행 시작 전 시작점 도달 여부 확인하는 상태
     // if 현재 위치 좌표 === StartPoint 노드 좌표 => setOnStartPoint(true)
     // 하는 로직 필요
@@ -81,67 +45,14 @@ function CourseDrivePage() {
     // console.log(coolList);
     // console.log(fillterList);
 
-    useEffect(() => {
-        let a =
-            lat1 +
-            lat2 +
-            lat3 +
-            lat4 +
-            lat5 +
-            lat6 +
-            lat7 +
-            lat8 +
-            lat9 +
-            lat10;
-        let b = a / 10;
-        setLat(b);
-
-        let c =
-            lng1 +
-            lng2 +
-            lng3 +
-            lng4 +
-            lng5 +
-            lng6 +
-            lng7 +
-            lng8 +
-            lng9 +
-            lng10;
-        let d = c / 10;
-        setLng(d);
-    }, [lat1, lat2, lat3, lat4, lat5, lat6, lat7, lat8, lat9, lat10]);
-
     return (
         <>
             <IntroductionModal />
             <div className="relative">
-                <LocationTest1 setLat1={setLat1} setLng1={setLng1} />
-                <LocationTest2 setLat2={setLat2} setLng2={setLng2} />
-                <LocationTest3 setLat3={setLat3} setLng3={setLng3} />
-                <LocationTest4 setLat4={setLat4} setLng4={setLng4} />
-                <LocationTest5 setLat5={setLat5} setLng5={setLng5} />
-                <LocationTest6 setLat6={setLat6} setLng6={setLng6} />
-                <LocationTest7 setLat7={setLat7} setLng7={setLng7} />
-                <LocationTest8 setLat8={setLat8} setLng8={setLng8} />
-                <LocationTest9 setLat9={setLat9} setLng9={setLng9} />
-                <LocationTest10 setLat10={setLat10} setLng10={setLng10} />
-                <WatchPosition1
-                    setWatchLat1={setWatchLat1}
-                    setWatchLng1={setWatchLng1}
-                />
-                <GoogleLocation
-                    setGoogleLat={setGoogleLat}
-                    setGoogleLng={setGoogleLng}
-                />
-                {/* <RealTimeCurrentLocation setLat={setLat} setLng={setLng} /> */}
-                {/* <ServerTest setcoolList={setcoolList} /> */}
-                {/* <Map lat={lat} lon={lon} /> */}
+                <RealTimeCurrentLocation setLat={setLat} setLng={setLng} />
+                <ServerTest setcoolList={setcoolList} />
                 <Topbar>코스 이름</Topbar>
-
-                <MapTest2 watchLat1={watchLat1} watchLng1={watchLng1} />
-                <MapTest3 googleLat={googleLat} googleLng={googleLng} />
-                <MapTest lat={lat} lng={lng} />
-                {/* <StopOver
+                <StopOver
                     lat={lat}
                     lng={lng}
                     coolList={coolList}
@@ -149,9 +60,8 @@ function CourseDrivePage() {
                     setFillterList={setFillterList}
                     setTime={setTime}
                     setKm={setKm}
-                /> */}
+                />
                 {/* UI 테스트용 임시 맵 */}
-                {/* <Map lat={lat} lon={lng} /> */}
                 <div className="fixed z-50 bottom-2 inset-x-0">
                     <Stack alignItems={"center"}>
                         {!isDriving ? (
