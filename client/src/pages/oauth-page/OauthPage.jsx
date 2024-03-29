f29bd1000e170faaba7216f92f967c55c772e61c
moonwlgh <moonwlgh@gmail.com>
2024-03-29 11:18:37 +0900

    Feat: sns로그인 구현

--- client/src/pages/oauth-page/OauthPage.jsx
+++ client/src/pages/oauth-page/OauthPage.jsx
@@ -1,7 +1,6 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
-import { jwtDecode } from "jwt-decode";
 
 const OauthPage = () => {
     const [user, setUser] = useState({});
@@ -12,7 +11,6 @@ const OauthPage = () => {
         const queryString = window.location.search;
         const urlParams = new URLSearchParams(queryString);
         const accessToken = urlParams.get("access");
-        console.log(accessToken);
         const refreshToken = urlParams.get("refresh");
 
         if (accessToken && refreshToken) {
@@ -20,24 +18,14 @@ const OauthPage = () => {
             localStorage.setItem("refreshToken", refreshToken);
         }
 
-        if (accessToken) {
-            try {
-                const decoded = jwtDecode(accessToken);
-                setUser(decoded); // 토큰에서 추출한 사용자 정보를 저장
-                console.log(decoded);
-                console.log(user);
-            } catch (error) {
-                console.error("Error decoding token:", error);
-            }
+        if (localStorage.getItem("accessToken")) {
+            navigate("/main");
+        } else {
+            navigate("/");
         }
     }, []);
 
-    // useEffect(() => {
-    //     if (user) navigate("/main");
-    //     else navigate("/");
-    // }, [user, navigate]);
-
-    return <div>Oauth Redirect Page</div>;
+    return <div>Oauth Redirect</div>;
 };
 
 export default OauthPage;
