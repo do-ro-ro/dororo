0b1e3b40ebc34759e3ec3b75efc020709d805265
문지호 <moonwlgh@gmail.com>
2024-03-30 15:09:31 +0900

    Fix: CurrentLocation.jsx lon => lng 변수명 변경

--- client/src/pages/main-page/CurrentLocation.jsx
+++ client/src/pages/main-page/CurrentLocation.jsx
@@ -1,11 +1,11 @@
 import GpsFixedIcon from "@mui/icons-material/GpsFixed";
 
-const CurrentLocation = ({ setLat, setLon, setTimestamp }) => {
+const CurrentLocation = ({ setLat, setLng, setTimestamp }) => {
     // 위치 정보를 가져오고 상태를 업데이트하는 함수
     const fetchCurrentPosition = () => {
         navigator.geolocation.getCurrentPosition((position) => {
             setLat(position.coords.latitude);
-            setLon(position.coords.longitude);
+            setLng(position.coords.longitude);
             setTimestamp(position.timestamp);
             console.log(position);
         });
