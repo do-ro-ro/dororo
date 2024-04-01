948ab255434fb0cd0bff49bdbe7e29112370fa49
jongmin Son <kain9101@naver.com>
2024-04-01 22:54:18 +0900

    Fix: 코스 상세 부분 맵 작업중...

--- client/src/components/course-detail/Map.jsx
+++ client/src/components/course-detail/Map.jsx
@@ -9,16 +9,8 @@ function Map({ course, courseName, toSave }) {
     const [markers, setMarkers] = useState([]);
     const [distance, setDistance] = useState(null);
 
-    // 선택한 마커 확인을 위한 상태
-    const [isSelected, setIsSelected] = useState(false);
-    const [selectedMarkerIndex, setSelectedMarkerIndex] = useState(null);
-    const [selectedMarkerPosition, setSelectedMarkerPosition] = useState(null);
-
     // polyline을 그리기 위한 보정된 path(꼭지점) 좌표를 저장하는 리스트
     const [courseLine, setCourseLine] = useState([]);
-
-    // viaPoint 설정을 위해 첫점과 끝점을 제외한 path 좌표를 저장하는 리스트
-    const [filteredCourse, setFilteredCourse] = useState([]);
 
     // 지도 위 거점을 찍기 위한 오리지널 노드 좌표를 저장하는 리스트
     const [courseNode, setCourseNode] = useState([]);
@@ -37,47 +29,6 @@ function Map({ course, courseName, toSave }) {
         currentCourse?.originMapRouteAxis[
             currentCourse.originMapRouteAxis.length - 1
         ];
-
-    // 처음 호출 시 APIparam
-    const basicAPIparam = {
-        startName: "출발지",
-        startX: `${basicStartPoint?.lng}`,
-        startY: `${basicStartPoint?.lat}`,
-        startTime: "202403201103",
-        endName: "도착지",
-        endX: `${basicEndPoint?.lng}`,
-        endY: `${basicEndPoint?.lat}`,
-        viaPoints: filteredCourse?.map((point) => ({
-            viaPointId: `test${filteredCourse?.indexOf(point) + 1}`,
-            viaPointName: `name${filteredCourse?.indexOf(point) + 1}`,
-            viaX: `${point.lng}`,
-            viaY: `${point.lat}`,
-        })),
-        reqCoordType: "WGS84GEO",
-        resCoordType: "EPSG3857",
-        searchOption: "2",
-    };
-
-    // 리셋 버튼을 위한 함수
-    const handleReset = async function () {
-        // 정리
-        if (polyline) {
-            polyline.setMap(null);
-            deleteMarkers();
-        }
-
-        await postRouteSequential30(basicAPIparam);
-        // courseLine이 존재하면 (경유지 탐색을 돌고 난 후라면)
-        drawPolyline();
-        if (markers.length === 0) {
-            drawMarkers();
-        }
-        drawMarkers();
-        drawPolyline();
-    };
-
-    // API 호출 확인을 위한 상태
-    const [checkAPI, setCheckAPI] = useState(0);
 
     // 코스정보를 받아왔을 때 웨이포인트는 보정 전 좌표로 등록하기
     useEffect(() => {
@@ -103,135 +54,6 @@ function Map({ course, courseName, toSave }) {
         });
 
         setMap(newMap);
-    };
-
-    const postRouteSequential30 = async (option) => {
-        // const searchOption = "2";
-
-        const headers = {
-            appKey: import.meta.env.VITE_TMAP_API_KEY,
-            "Content-Type": "application/json",
-        };
-
-        const param = option;
-        // console.log("API 호출용 param", param);
-
-        fetch(
-            "https://apis.openapi.sk.com/tmap/routes/routeSequential30?version=1&format=json",
-            // "https://apis.openapi.sk.com/tmap/routes/routeSequential100?version=1&format=json",
-            // "https://apis.openapi.sk.com/tmap/routes/routeSequential200?version=1&format=json",
-
-            {
-                method: "POST",
-                headers: headers,
-                body: JSON.stringify(param),
-            },
-        )
-            .then((response) => response.json())
-            .then((data) => {
-                console.log("호출 완료!");
-                setCourseLine([]);
-                // console.log(data);
-                // 총 거리, 시간, 요금이 담겨있는 데이터
-                const resultData = data.properties;
-
-                // 경유지 위치 정보들이 담겨 있는 배열
-                const resultFeatures = data.features;
-                // console.log(resultFeatures);
-
-                const tDistance = (resultData?.totalDistance / 1000).toFixed(1);
-
-                // 거리 저장
-                setDistance(tDistance);
-                // const tTime =
-                //     "총 시간 : " +
-                //     (resultData.totalTime / 60).toFixed(0) +
-                //     "분,  ";
-                // const tFare = "총 요금 : " + resultData.totalFare + "원";
-
-                // resultInfoArr에 값이 존재하면, 갱신할 것
-                if (resultInfoArr?.length > 0) {
-                    setResultInfoArr([]);
-                }
-                setResultMarkerArr([]);
-
-                // resultFeatures 의 정보 추출하기 위한 forEach문
-                resultFeatures?.forEach((feature) => {
-                    const geometry = feature.geometry;
-                    const properties = feature.properties;
-                    // console.log(geometry);
-
-                    // 해당 정보 타입이 LineString(링크) 일 경우
-                    if (geometry.type === "LineString") {
-                        const drawInfoArr = geometry.coordinates.map(
-                            (coord) => {
-                                const latlng = new window.Tmapv2.Point(
-                                    coord[0],
-                                    coord[1],
-                                );
-                                const convertPoint =
-                                    new window.Tmapv2.Projection.convertEPSG3857ToWGS84GEO(
-                                        latlng,
-                                    );
-
-                                // 코스라인 상태에 lineString인 좌표들 담아주기
-                                setCourseLine((prev) => [
-                                    ...prev,
-                                    convertPoint,
-                                ]);
-
-                                const pathAxis = {
-                                    lat: convertPoint._lat,
-                                    lng: convertPoint._lng,
-                                };
-                                // 변환한 좌표 담기 위한 리스트 지정
-                                setResultInfoArr((prev) => [...prev, pathAxis]);
-                                return new window.Tmapv2.LatLng(
-                                    convertPoint._lat,
-                                    convertPoint._lng,
-                                );
-                            },
-                        );
-                    }
-                    if (geometry.type !== "LineString") {
-                        // type이 Point(노드)일 경우
-                        let markerImg = "";
-                        let size = "";
-                        let draggable = false;
-
-                        if (properties.pointType === "S") {
-                            // 시작점이면
-                            markerImg = startPin;
-                            size = new window.Tmapv2.Size(24, 38);
-                        } else if (properties.pointType === "E") {
-                            // 종착점이면
-                            markerImg = endPin;
-                            size = new window.Tmapv2.Size(24, 38);
-                        } else {
-                            // 거쳐가는 점이면
-                            markerImg = waypointPin;
-                            size = new window.Tmapv2.Size(24, 24);
-                            draggable = true;
-                        }
-
-                        const latlng = new window.Tmapv2.Point(
-                            geometry.coordinates[0],
-                            geometry.coordinates[1],
-                        );
-                        const convertPoint =
-                            new window.Tmapv2.Projection.convertEPSG3857ToWGS84GEO(
-                                latlng,
-                            );
-
-                        // 마커값 결과 상태에 담아주기
-                        setResultMarkerArr((prev) => [...prev, convertPoint]);
-                    }
-                });
-                setCheckAPI((prev) => prev + 1);
-            })
-            .catch((error) => {
-                console.error("Error:", error);
-            });
     };
 
     // 라인 그리는 함수
