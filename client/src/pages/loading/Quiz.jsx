4b8bed5d6541fc23843866fd28b67cbca7b4f1e1
문지호 <moonwlgh@gmail.com>
2024-04-01 23:11:34 +0900

    Feat: 퀴즈 기능 구현

--- client/src/pages/loading/Quiz.jsx
+++ client/src/pages/loading/Quiz.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Box, Button, Typography } from "@mui/material";
 
 const Quiz = () => {
@@ -15,30 +15,11 @@ const Quiz = () => {
             correctAnswer: "X",
             explanation: "HTML은 마크업 언어이며, 프로그래밍 언어가 아닙니다.",
         },
-        // 추가 질문들...
     ];
 
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
     const [selectedAnswer, setSelectedAnswer] = useState("");
     const [explanation, setExplanation] = useState("");
-
-    useEffect(() => {
-        let timer;
-        if (
-            selectedAnswer &&
-            selectedAnswer !== quizData[currentQuestionIndex].correctAnswer
-        ) {
-            timer = setTimeout(() => {
-                if (currentQuestionIndex < quizData.length - 1) {
-                    setCurrentQuestionIndex(currentQuestionIndex + 1);
-                    setSelectedAnswer("");
-                    setExplanation("");
-                }
-            }, 5000);
-        }
-
-        return () => clearTimeout(timer); // 컴포넌트 언마운트 시 타이머 정리
-    }, [selectedAnswer, currentQuestionIndex, quizData]);
 
     const currentQuestion = quizData[currentQuestionIndex];
 
@@ -49,10 +30,7 @@ const Quiz = () => {
         } else {
             setExplanation("");
             if (currentQuestionIndex < quizData.length - 1) {
-                setTimeout(
-                    () => setCurrentQuestionIndex(currentQuestionIndex + 1),
-                    5000,
-                );
+                setCurrentQuestionIndex(currentQuestionIndex + 1);
             }
         }
     };
